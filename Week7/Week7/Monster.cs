fa5f82b298ab67e45bc20778980a0f160153e11e
knightboy13 <thefirstknight8th@gmail.com>
2026-03-18 12:55:02 +1100

    Week 7: read and write text files and monster game

---
+++ Week7/Week7/Monster.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Week7
+{
+    class Monster
+    {
+        public string name = "Monster";
+        public int health;
+        public int attackDamage;
+
+        public Monster()
+        {
+            health = 50;
+            attackDamage = 2;
+        }
+
+        public Monster(string name, int health, int attackDamage)
+        {
+            this.name = name;
+            this.health = health;
+            this.attackDamage = attackDamage;
+        }
+
+        public int Attack()
+        {
+
+            int damage = attackDamage;
+
+            Random randomNum = new Random();
+
+            damage -= randomNum.Next(0, 3);
+            Console.WriteLine(name + " winds up for an attack ");
+            return damage;
+        }
+
+        public bool TakeDamage(int damage)
+        {
+
+            health -= damage;
+            Console.WriteLine(name + " took " + damage + " damage. " + health + " health remaining");
+
+            if(health <= 0)
+            {
+                Console.WriteLine(name + " has kicked the bucket");
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ Week7/Week7/Program.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Monster monsterOne = new Monster();
+            //Monster monsterTwo = new Monster("Barry", 60, 5);
+
+            Monster[] monsters = new Monster[2];
+
+
+
+            string configFile = "monster_stats.txt";
+
+            if (File.Exists(configFile))
+            {
+                LoadMonsterStats(monsters, configFile);
+                Console.WriteLine("Loaded your custom monsters!");
+            }
+            else
+            {
+                monsters[0] = new Monster();
+                monsters[1] = new Monster("Barry", 60, 5);
+
+                SaveMonsterStats(monsters, configFile);
+                Console.WriteLine("Created monster_stats.txt");
+            }
+
+
+
+            bool isFighting = true;
+
+            int currentMonsterIndex = 0;
+            int otherMonsterIndex = 1;
+            while (isFighting == true)
+            {
+                string input = Console.ReadLine();
+                
+
+                if (input == "attack")
+                {
+                    Console.WriteLine("Player chose attack for Monster!");
+                }
+                int attackDamage = monsters[currentMonsterIndex].Attack();
+
+                if (monsters[otherMonsterIndex].TakeDamage(attackDamage) == true)
+                {
+                    isFighting = false;
+                }
+
+                currentMonsterIndex++;
+
+                if (currentMonsterIndex >= monsters.Length)
+                {
+                    currentMonsterIndex = 0;
+                }
+
+                otherMonsterIndex++;
+                if (otherMonsterIndex >= monsters.Length)
+                {
+                    otherMonsterIndex = 1;
+                }
+            }
+            Console.ReadKey();
+
+
+        }
+
+        static void SaveMonsterStats(Monster[] monsters, string fileName)
+        {
+            StreamWriter file = new StreamWriter(fileName);
+
+            // save first monster
+            file.WriteLine(monsters[0].name);
+            file.WriteLine(monsters[0].health);
+            file.WriteLine(monsters[0].attackDamage);
+
+            // save Barry
+            file.WriteLine(monsters[1].name);
+            file.WriteLine(monsters[1].health);
+            file.WriteLine(monsters[1].attackDamage);
+
+            file.Close();
+        }
+
+        static void LoadMonsterStats(Monster[] monsters, string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+
+            //load first monster (lines 0, 1, 2)
+            monsters[0] = new Monster(lines[0], int.Parse(lines[1]), int.Parse(lines[2]));
+
+            // load Barry (lines 0, 1, 2)
+            monsters[1] = new Monster(lines[3], int.Parse(lines[4]), int.Parse(lines[5]));
+
+        }
+    }
+}
